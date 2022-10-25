ceebf4cf358539ad034b769b2797f5e774cb380e
ahadhasanjion <niallhoran3176@gmail.com>
2022-10-25 17:58:58 +0600

    added

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ const port = 5000;
 const cors = require('cors');
 
 app.use(cors())
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
+
+app.get('/', (req, res) => {
+    res.send('Nws API Running');
+});
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id==="06"){
+        res.send(news);
+    }
+    else{
+        const category_courses = news.filter(n => n.category_id === id);
+    res.send(category_courses);
+
+    }
+})
+
+app.get('/courses', (req, res)=> {
+    res.send(news);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = news.find(n => n._id === id);
+    res.send(selectedCourses);
+})
+
+app.listen(port, () => {
+    console.log('HELLO NEWS', port);
+});
