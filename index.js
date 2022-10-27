d1fcbb1d5916fc4af08f73a7c73ec77f67d3cf66
ahadhasanjion <niallhoran3176@gmail.com>
2022-10-27 13:33:29 +0600

    vercel

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 const cors = require('cors');
+const port = process.env.PORT || 5000;
+
 
 app.use(cors())
-const categories = require('./data/categories.json');
-const courses = require('./data/courses.json');
+
+const course = require('./Data/course.json');
 
 app.get('/', (req, res) => {
     res.send('Nws API Running');
 });
 
-app.get('/course-categories', (req, res) => {
-    res.send(categories);
+
+app.get('/course', (req, res)=> {
+    res.send(course);
 })
 
-app.get('/category/:id', (req, res) => {
+app.get('/course/:id', (req, res) => {
     const id = req.params.id;
-    if(id==="06"){
-        res.send(news);
-    }
-    else{
-        const category_courses = news.filter(n => n.category_id === id);
-    res.send(category_courses);
-
-    }
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
 })
 
-app.get('/courses', (req, res)=> {
-    res.send(news);
-})
-
-app.get('/courses/:id', (req, res) => {
+app.get('/premium/:id', (req, res) => {
     const id = req.params.id;
-    const selectedCourses = news.find(n => n._id === id);
-    res.send(selectedCourses);
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
